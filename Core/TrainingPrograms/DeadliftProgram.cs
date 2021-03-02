85ab009575991522f833a83f880ef76209422310
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-03-02 23:05:24 +0400

    Deadlift, day 32

--- Core/TrainingPrograms/DeadliftProgram.cs
+++ Core/TrainingPrograms/DeadliftProgram.cs
@@ -36,7 +36,7 @@ namespace Core.TrainingPrograms
                 [++key] = GetSession8_1, [++key] = GetSession8_2, [++key] = GetSession8_3,
                 [++key] = GetSession9_1, [++key] = GetSession9_2, [++key] = GetSession9_3,
                 [++key] = GetSession10_1, [++key] = GetSession10_2, [++key] = GetSession10_3,
-                [++key] = GetSession11_1
+                [++key] = GetSession11_1, [++key] = GetSession11_2
             };
 
             return result;
@@ -1736,6 +1736,45 @@ namespace Core.TrainingPrograms
                     new(ex3),
                     new(ex4),
                     new(ex5)
+                }
+            };
+
+            return session;
+        }
+
+        private static Session GetSession11_2()
+        {
+            var ex1 = new Accessory(Hyperextension)
+            {
+                Repeats = new[]
+                {
+                    new Repeat {Repeats = "20", Sets = 2}
+                }
+            };
+
+            var ex2 = new Snatch(BaseSnatch)
+            {
+                Repeats = new Repeat[]
+                {
+                    new() {Percent = 0.4, Repeats = "3", Sets = 3}
+                }
+            };
+
+            var ex3 = new CleanAndJerk(Clean, Jerk)
+            {
+                Repeats = new Repeat[]
+                {
+                    new() {Percent = 0.4, Repeats = "2+2", Sets = 3}
+                }
+            };
+
+            var session = new Session
+            {
+                Sets = new Set[]
+                {
+                    new(ex1),
+                    new(ex2),
+                    new(ex3)
                 }
             };
 
