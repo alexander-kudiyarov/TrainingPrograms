b73987a9228ac2ec55d0784ceee8ebd75dc90f16
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-03-27 12:14:27 +0400

    Program.cs

--- ConsoleUi/Program.cs
+++ ConsoleUi/Program.cs
@@ -1,9 +1,29 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Core.Bll;
+using Core.Bll.Interfaces;
+using Core.Dal;
+using Core.Entities;
+using Core.Entities.Enums;
 
 namespace ConsoleUi
 {
+    [MemoryDiagnoser]
     public abstract class Benchmark
     {
+        private readonly ITrainingProgramLogic _logic;
+
+        protected Benchmark()
+        {
+            var repository = new ProgramRepository();
+            _logic = new TrainingProgramLogic(repository);
+        }
+
+        [Benchmark]
+        public Session GetSession()
+        {
+            return _logic.Get(ProgramType.LegsProgram, 3);
+        }
     }
 
     internal static class Program
