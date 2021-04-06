afbb21b0dcb5ae618aba18a4daec16cebcd0288c
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-04-06 20:32:10 +0400

    Legs, day 13

--- Core/TrainingPrograms/LegsProgram.cs
+++ Core/TrainingPrograms/LegsProgram.cs
@@ -14,7 +14,8 @@ namespace Core.TrainingPrograms
         {
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4,
-            GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4
+            GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4,
+            GetSession4_1
         };
 
         public LegsProgram() : base(Sessions)
@@ -878,6 +879,97 @@ namespace Core.TrainingPrograms
                     new(3, ex1A, ex1B, ex1C),
                     new(ex2),
                     new(ex3)
+                }
+            };
+
+            return session;
+        }
+
+        private static Session GetSession4_1()
+        {
+            var ex1A = new Accessory(Hyperextension)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 15}
+                }
+            };
+
+            var ex1B = new Accessory(Crunch)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 15}
+                }
+            };
+
+            var ex1C = new Accessory(GakkSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 15}
+                }
+            };
+
+            var ex2 = new Snatch(PowerSnatch, OverheadSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new MultiRepeat {Percent = 0.5, Repeats = new[] {3, 3}, Sets = 4}
+                }
+            };
+
+            var ex3 = new BackSquat(ExerciseType.BackSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Percent = 0.5, Repeats = 3},
+                    new SingleRepeat {Percent = 0.6, Repeats = 3},
+                    new SingleRepeat {Percent = 0.7, Repeats = 3, Sets = 2},
+                    new SingleRepeat {Percent = 0.8, Repeats = 2},
+                    new SingleRepeat {Percent = 0.85, Repeats = 2, Sets = 2},
+                    new SingleRepeat {Percent = 0.9, Repeats = 2, Sets = 2},
+                    new SingleRepeat {Percent = 0.925, Repeats = 1, Sets = 2}
+                }
+            };
+
+            var ex4 = new BackSquat(BenchBackSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Percent = 0.7, Repeats = 2},
+                    new SingleRepeat {Percent = 0.8, Repeats = 2},
+                    new SingleRepeat {Percent = 0.9, Repeats = 2},
+                    new SingleRepeat {Percent = 0.95, Repeats = 2, Sets = 3}
+                }
+            };
+
+            var ex5 = new Accessory(GoodMorningSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Percent = 0.4, Repeats = 6, Sets = 3}
+                }
+            };
+
+            var ex6 = new Accessory(BarbellSquatJump)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 6, Sets = 5}
+                }
+            };
+
+            var session = new Session
+            {
+                Rounds = new Round[]
+                {
+                    new(3, ex1A, ex1B, ex1C),
+                    new(ex2),
+                    new(ex3),
+                    new(ex4),
+                    new(ex5),
+                    new(ex6)
                 }
             };
 
