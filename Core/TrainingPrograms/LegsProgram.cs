794b72909dbd1da1b926a73824e18cf407e07b64
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-04-12 20:30:39 +0400

    Legs, day 16

--- Core/TrainingPrograms/LegsProgram.cs
+++ Core/TrainingPrograms/LegsProgram.cs
@@ -15,7 +15,7 @@ namespace Core.TrainingPrograms
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4,
             GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4,
-            GetSession4_1, GetSession4_2, GetSession4_3
+            GetSession4_1, GetSession4_2, GetSession4_3, GetSession4_4
         };
 
         public LegsProgram() : base(Sessions)
@@ -1135,6 +1135,63 @@ namespace Core.TrainingPrograms
                     new(ex3),
                     new(ex4),
                     new(ex5)
+                }
+            };
+
+            return session;
+        }
+
+        private static Session GetSession4_4()
+        {
+            var ex1A = new Accessory(Hyperextension)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 10}
+                }
+            };
+
+            var ex1B = new Accessory(Crunch)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 10}
+                }
+            };
+
+            var ex1C = new Accessory(BoxJump)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 10}
+                }
+            };
+
+            var ex2 = new Snatch(ExerciseType.Snatch)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Percent = 0.5, Repeats = 3, Sets = 2},
+                    new SingleRepeat {Percent = 0.6, Repeats = 2, Sets = 3}
+                }
+            };
+
+            var ex3 = new CleanAndJerk(Clean, Jerk)
+            {
+                Repeats = new Repeat[]
+                {
+                    new MultiRepeat {Percent = 0.5, Repeats = new[] {2, 2}, Sets = 2},
+                    new MultiRepeat {Percent = 0.6, Repeats = new[] {2, 2}, Sets = 3}
+                }
+            };
+
+            var session = new Session
+            {
+                Rounds = new Round[]
+                {
+                    new(3, ex1A, ex1B, ex1C),
+                    new(ex2),
+                    new(ex3)
                 }
             };
 
