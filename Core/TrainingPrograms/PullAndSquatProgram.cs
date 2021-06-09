517399af7c09b75717d7fcbae4072643a29ca25b
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-06-09 20:28:42 +0400

    PullAndSquat, day 20

--- Core/TrainingPrograms/PullAndSquatProgram.cs
+++ Core/TrainingPrograms/PullAndSquatProgram.cs
@@ -15,7 +15,7 @@ namespace Core.TrainingPrograms
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4, GetSession1_5,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4, GetSession2_5,
             GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4, GetSession3_5,
-            GetSession4_1, GetSession4_2, GetSession4_3, GetSession4_4
+            GetSession4_1, GetSession4_2, GetSession4_3, GetSession4_4, GetSession4_5
         };
 
         public PullAndSquatProgram() : base(Sessions)
@@ -1201,6 +1201,67 @@ namespace Core.TrainingPrograms
                 {
                     new(2, ex1A, ex1B, ex1C),
                     new(ex2)
+                }
+            };
+
+            return session;
+        }
+
+        private static Session GetSession4_5()
+        {
+            var ex1A = new Accessory(Hyperextension)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 15}
+                }
+            };
+
+            var ex1B = new Accessory(Crunch)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Repeats = 20}
+                }
+            };
+
+            var ex2 = new CleanAndJerk(FrontSquat, PushPress)
+            {
+                Repeats = new[]
+                {
+                    new MultiRepeat {Percent = 0.5, Repeats = new[] {2, 3}, Sets = 2},
+                    new MultiRepeat {Percent = 0.6, Repeats = new[] {1, 3}, Sets = 3}
+                }
+            };
+
+            var ex3 = new BackSquat(ExerciseType.BackSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Percent = 0.5, Repeats = 3, Sets = 2},
+                    new SingleRepeat {Percent = 0.6, Repeats = 3, Sets = 1},
+                    new SingleRepeat {Percent = 0.7, Repeats = 3, Sets = 2},
+                    new SingleRepeat {Percent = 0.8, Repeats = 3, Sets = 1},
+                    new SingleRepeat {Percent = 0.85, Repeats = 2, Sets = 2}
+                }
+            };
+
+            var ex4 = new BackSquat(BenchBackSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat {Percent = 0.85, Repeats = 6, Sets = 2}
+                }
+            };
+
+            var session = new Session
+            {
+                Rounds = new Round[]
+                {
+                    new(3, ex1A, ex1B),
+                    new(ex2),
+                    new(ex3),
+                    new(ex4)
                 }
             };
 
