b6536c8ab1711485e52d9f804e3c48919cc96e4b
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-10-19 19:48:07 +0400

    Solution splitted to different projects

---
+++ Kudiyarov.TrainingPrograms.Di/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Kudiyarov.TrainingPrograms.Core.Bll;
+using Kudiyarov.TrainingPrograms.Core.Bll.Interfaces;
+using Kudiyarov.TrainingPrograms.Core.Dal;
+using Kudiyarov.TrainingPrograms.Core.Dal.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kudiyarov.TrainingPrograms.Di
+{
+    public static class Bootstrapper
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ITrainingProgramLogic, TrainingProgramLogic>();
+            services.AddSingleton<IProgramRepository, ProgramRepository>();
+        }
+    }
+}
--- Kudiyarov.TrainingPrograms.Web/Startup.cs
+++ Kudiyarov.TrainingPrograms.Web/Startup.cs
@@ -2,6 +2,7 @@ using Kudiyarov.TrainingPrograms.Core.Bll;
 using Kudiyarov.TrainingPrograms.Core.Bll.Interfaces;
 using Kudiyarov.TrainingPrograms.Core.Dal;
 using Kudiyarov.TrainingPrograms.Core.Dal.Interfaces;
+using Kudiyarov.TrainingPrograms.Di;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +16,7 @@ namespace Kudiyarov.TrainingPrograms.Web
         {
             services.AddControllersWithViews();
             services.AddMemoryCache();
-            services.AddSingleton<ITrainingProgramLogic, TrainingProgramLogic>();
-            services.AddSingleton<IProgramRepository, ProgramRepository>();
+            services.AddApplicationServices();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
