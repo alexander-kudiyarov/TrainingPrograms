d358cce984820f037b8871e14c700bc5aa7dae30
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-10-19 19:23:04 +0400

    Competition, day 56

--- Kudiyarov.TrainingPrograms.Core/Common/TrainingPrograms/CompetitionProgram.cs
+++ Kudiyarov.TrainingPrograms.Core/Common/TrainingPrograms/CompetitionProgram.cs
@@ -24,7 +24,8 @@ namespace Kudiyarov.TrainingPrograms.Core.Common.TrainingPrograms
             GetSession8_1, GetSession8_2, GetSession8_3, GetSession8_4, GetSession8_5,
             GetSession9_1, GetSession9_2, GetSession9_3, GetSession9_4, GetSession9_5,
             GetSession10_1, GetSession10_2, GetSession10_3, GetSession10_4, GetSession10_5,
-            GetSession11_1, GetEmptySession, GetSession11_3, GetSession11_4, GetSession11_5
+            GetSession11_1, GetEmptySession, GetSession11_3, GetSession11_4, GetSession11_5,
+            GetSession12_1
         };
 
         public CompetitionProgram() : base(Sessions)
@@ -3338,6 +3339,59 @@ namespace Kudiyarov.TrainingPrograms.Core.Common.TrainingPrograms
                 {
                     new(ex1),
                     new(ex2)
+                }
+            };
+
+            return session;
+        }
+
+        private static Session GetSession12_1()
+        {
+            var ex1 = new Snatch(PowerSnatch)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat { Percent = 0.50, Repeats = 2, Sets = 2 },
+                    new SingleRepeat { Percent = 0.60, Repeats = 2, Sets = 1 },
+                    new SingleRepeat { Percent = 0.65, Repeats = 2, Sets = 1 },
+                    new SingleRepeat { Percent = 0.70, Repeats = 2, Sets = 2 },
+                    new SingleRepeat { Percent = 0.75, Repeats = 1, Sets = 3 }
+                }
+            };
+
+            var ex2 = new MultiCleanAndJerk(Clean, Jerk)
+            {
+                Repeats = new Repeat[]
+                {
+                    new MultiRepeat { Percent = 0.50, Repeats = Array(2, 2), Sets = 2 },
+                    new MultiRepeat { Percent = 0.60, Repeats = Array(2, 2), Sets = 1 },
+                    new MultiRepeat { Percent = 0.65, Repeats = Array(2, 2), Sets = 1 },
+                    new MultiRepeat { Percent = 0.70, Repeats = Array(1, 2), Sets = 1 },
+                    new MultiRepeat { Percent = 0.70, Repeats = Array(2, 1), Sets = 1 },
+                    new MultiRepeat { Percent = 0.75, Repeats = Array(1, 1), Sets = 2 },
+                    new MultiRepeat { Percent = 0.80, Repeats = Array(1, 1), Sets = 2 }
+                }
+            };
+
+            var ex3 = new CleanAndJerk(FrontSquat)
+            {
+                Repeats = new Repeat[]
+                {
+                    new SingleRepeat { Percent = 0.50, Repeats = 3, Sets = 2 },
+                    new SingleRepeat { Percent = 0.60, Repeats = 3, Sets = 1 },
+                    new SingleRepeat { Percent = 0.70, Repeats = 3, Sets = 2 },
+                    new SingleRepeat { Percent = 0.80, Repeats = 2, Sets = 1 },
+                    new SingleRepeat { Percent = 0.85, Repeats = 2, Sets = 3 }
+                }
+            };
+
+            var session = new Session
+            {
+                Rounds = new Round[]
+                {
+                    new(ex1),
+                    new(ex2),
+                    new(ex3)
                 }
             };
 
