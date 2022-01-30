34395f858121389a2e60fadc567c201ccc54258f
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-01-30 21:09:55 +0400

    Clean, day 9

--- Kudiyarov.TrainingPrograms.Entities/TrainingPrograms/Technique/CleanProgram.cs
+++ Kudiyarov.TrainingPrograms.Entities/TrainingPrograms/Technique/CleanProgram.cs
@@ -19,7 +19,7 @@ public class CleanProgram : BackTechniqueProgram
         var sessions = new Func<Session>[]
         {
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4, GetSession1_5,
-            GetSession2_1, GetSession2_2, GetSession2_3
+            GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4
         };
 
         return sessions;
@@ -452,7 +452,7 @@ public class CleanProgram : BackTechniqueProgram
             }
         };
 
-        var ex3 = new MultiCleanAndJerk(DeficitCleanPull)
+        var ex3 = new CleanAndJerk(DeficitCleanPull)
         {
             Repeats = new Repeat[]
             {
@@ -481,6 +481,68 @@ public class CleanProgram : BackTechniqueProgram
                 new(ex2),
                 new(ex3),
                 new(ex4)
+            }
+        };
+
+        return session;
+    }
+
+    private static Session GetSession2_4()
+    {
+        var warmup = GetWarmup2(15, 15, 5);
+
+        var ex2A = new Accessory(ElbowsRotation)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Weight = Stats.Bar, Repeats = 8 }
+            }
+        };
+
+        var ex2B = new Accessory(WallSquat)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 8 }
+            }
+        };
+
+        var ex3 = new CleanAndJerk(Jerk)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.5, Repeats = 3, Sets = 2 },
+                new SingleRepeat { Percent = 0.6, Repeats = 3, Sets = 2 },
+                new SingleRepeat { Percent = 0.7, Repeats = 2, Sets = 2 },
+                new SingleRepeat { Percent = 0.8, Repeats = 1, Sets = 2 }
+            }
+        };
+
+        var ex4 = new CleanAndJerk(GoodMorning)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.5, Repeats = 6, Sets = 3 }
+            }
+        };
+
+        var ex5 = new Accessory(ReversePlank)
+        {
+            Repeats = new Repeat[]
+            {
+                new StaticRepeat { Duration = Duration.FromSeconds(60), Sets = 3 }
+            }
+        };
+
+        var session = new Session
+        {
+            Rounds = new Round[]
+            {
+                new(3, warmup),
+                new(2, ex2A, ex2B),
+                new(ex3),
+                new(ex4),
+                new(ex5)
             }
         };
 
