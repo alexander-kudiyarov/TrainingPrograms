5e4b591fa2cdb362fdfdcb5c62cf6cb13470785e
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-02-16 21:20:40 +0400

    Clean, day 14

--- Kudiyarov.TrainingPrograms.Entities/TrainingPrograms/Technique/CleanProgram.cs
+++ Kudiyarov.TrainingPrograms.Entities/TrainingPrograms/Technique/CleanProgram.cs
@@ -20,7 +20,7 @@ public class CleanProgram : BackTechniqueProgram
         {
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4, GetSession1_5,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4, GetSession2_5,
-            GetSession3_1, GetSession3_2, GetSession3_3
+            GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4
         };
 
         return sessions;
@@ -833,6 +833,48 @@ public class CleanProgram : BackTechniqueProgram
                 new(ex4),
                 new(ex5),
                 new(ex6)
+            }
+        };
+
+        return session;
+    }
+
+    private static Session GetSession3_4()
+    {
+        var warmup = GetWarmup1(15, 15, 5);
+
+        var ex1 = new MultiSnatch(MuscleSquatSnatch, OverheadSquat)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Percent = 0.5, Repeats = Array(3, 3), Sets = 3 }
+            }
+        };
+
+        var ex2 = new Snatch(ExerciseType.Snatch)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.5, Repeats = 3, Sets = 6 }
+            }
+        };
+
+        var ex3 = new Accessory(ReversePlank)
+        {
+            Repeats = new Repeat[]
+            {
+                new StaticRepeat { Duration = Duration.FromSeconds(60), Sets = 3 }
+            }
+        };
+
+        var session = new Session
+        {
+            Rounds = new Round[]
+            {
+                new(3, warmup),
+                new(ex1),
+                new(ex2),
+                new(ex3)
             }
         };
 
