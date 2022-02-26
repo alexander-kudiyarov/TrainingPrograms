4496fd51e730de9e880e91dffa5f4c383296d56f
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-02-26 15:14:39 +0400

    MapDefaultControllerRoute usage

--- Kudiyarov.TrainingPrograms.Web/Program.cs
+++ Kudiyarov.TrainingPrograms.Web/Program.cs
@@ -14,9 +14,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting();
-app.UseEndpoints(endpoints =>
-    endpoints.MapControllerRoute(
-        "default",
-        "{controller=Home}/{action=Index}/{id?}"));
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 app.Run();
