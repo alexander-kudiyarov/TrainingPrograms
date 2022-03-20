1042f0452b870fbb27eb0c1045272f85a67cd83c
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-03-20 10:37:35 +0400

    SessionRequest class -> record

--- Kudiyarov.TrainingPrograms.Entities/Requests/SessionRequest.cs
+++ Kudiyarov.TrainingPrograms.Entities/Requests/SessionRequest.cs
@@ -2,25 +2,8 @@ using Kudiyarov.TrainingPrograms.Entities.Enums;
 
 namespace Kudiyarov.TrainingPrograms.Entities.Requests;
 
-public class SessionRequest
+public record SessionRequest
 {
     public ProgramType ProgramType { get; init; }
     public int Day { get; init; }
-
-    public override bool Equals(object? other)
-    {
-        return other is SessionRequest request
-               && Equals(request);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(ProgramType, Day);
-    }
-
-    private bool Equals(SessionRequest other)
-    {
-        return ProgramType == other.ProgramType
-               && Day == other.Day;
-    }
 }
