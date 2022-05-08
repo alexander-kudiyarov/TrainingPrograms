94ee5ce34b97c28a1c1a0c43dd325e8689d8cb45
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-05-08 20:40:04 +0400

    Speed, day 2

--- Kudiyarov.TrainingPrograms.Programs/Technique/SpeedProgram.cs
+++ Kudiyarov.TrainingPrograms.Programs/Technique/SpeedProgram.cs
@@ -1,6 +1,7 @@
 ﻿using Kudiyarov.TrainingPrograms.Entities;
 using Kudiyarov.TrainingPrograms.Entities.Enums;
 using Kudiyarov.TrainingPrograms.Entities.Exercises;
+using Kudiyarov.TrainingPrograms.Entities.Exercises.CleanAndJerk;
 using Kudiyarov.TrainingPrograms.Entities.Exercises.Snatch;
 using Kudiyarov.TrainingPrograms.Entities.Repeats;
 using static Kudiyarov.TrainingPrograms.Entities.Enums.ExerciseType;
@@ -16,7 +17,7 @@ public class SpeedProgram : TrainingProgram
     {
         var sessions = new Func<Session>[]
         {
-            GetSession1_1
+            GetSession1_1, GetSession1_2
         };
 
         return sessions;
@@ -82,6 +83,79 @@ public class SpeedProgram : TrainingProgram
                 new SingleRepeat { Percent = 0.60, Repeats = 3, Sets = 1 },
                 new SingleRepeat { Percent = 0.70, Repeats = 3, Sets = 2 },
                 new SingleRepeat { Percent = 0.75, Repeats = 2, Sets = 2 }
+            }
+        };
+
+        var session = new Session
+        {
+            Rounds = new Round[]
+            {
+                new(2, warmup),
+                new(ex1),
+                new(ex2),
+                new(ex3)
+            }
+        };
+
+        return session;
+    }
+
+    private static Session GetSession1_2()
+    {
+        var a = new Accessory(Hyperextension)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 10 }
+            }
+        };
+
+        var b = new Accessory(GakkSquat)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 15 }
+            }
+        };
+
+        var c = new CleanAndJerk(HipCleanBalance)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Weight = Constants.Pvc, Repeats = 6 }
+            }
+        };
+
+        var warmup = new BaseExercise[]
+        {
+            a, b, c
+        };
+
+        var ex1 = new MultiCleanAndJerk(CleanFromBlocks, FrontSquat)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Percent = 0.5, Repeats = Array(3, 2), Sets = 2 },
+                new MultiRepeat { Percent = 0.6, Repeats = Array(3, 2), Sets = 2 },
+                new MultiRepeat { Percent = 0.7, Repeats = Array(2, 2), Sets = 3 }
+            }
+        };
+
+        var ex2 = new CleanAndJerk(CleanPullFromBlocks)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.7, Repeats = 3, Sets = 2 },
+                new SingleRepeat { Percent = 0.8, Repeats = 3, Sets = 2 },
+                new SingleRepeat { Percent = 0.9, Repeats = 2, Sets = 2 }
+            }
+        };
+
+        var ex3 = new Accessory(Press)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Intensity = Intensity.Light, Repeats = 5, Sets = 3 }
             }
         };
 
