98da8620bb1ca4edc1ffff05ef3e0a0e99ce5942
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-06-14 21:50:01 +0400

    Speed, day 16

--- Kudiyarov.TrainingPrograms.Programs/Technique/SpeedProgram.cs
+++ Kudiyarov.TrainingPrograms.Programs/Technique/SpeedProgram.cs
@@ -20,7 +20,8 @@ public class SpeedProgram : TrainingProgram
         {
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4, GetSession1_5,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4, GetSession2_5,
-            GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4, GetSession3_5
+            GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4, GetSession3_5,
+            GetSession4_1
         };
 
         return sessions;
@@ -1173,6 +1174,88 @@ public class SpeedProgram : TrainingProgram
             Repeats = new Repeat[]
             {
                 new SingleRepeat { Repeats = 8, Sets = 3 }
+            }
+        };
+
+        var session = new Session
+        {
+            Rounds = new Round[]
+            {
+                new(2, warmup),
+                new(ex1),
+                new(ex2),
+                new(ex3)
+            }
+        };
+
+        return session;
+    }
+
+    private static Session GetSession4_1()
+    {
+        var a = new Accessory(GoodMorning)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 8 }
+            }
+        };
+
+        var b = new Accessory(BoxJump)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 8 }
+            }
+        };
+
+        var c = new MultiSnatch(MuscleSquatSnatch, SnatchBalance)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Weight = Constants.Pvc, Repeats = Array(6, 6) }
+            }
+        };
+
+        var warmup = new BaseExercise[]
+        {
+            a, b, c
+        };
+
+        var ex1 = new MultiSnatch(ExerciseType.Snatch, PowerSnatch, SnatchBalance)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Percent = 0.50, Repeats = Array(2, 1, 2), Sets = 2 },
+                new MultiRepeat { Percent = 0.60, Repeats = Array(2, 1, 2), Sets = 1 },
+                new MultiRepeat { Percent = 0.70, Repeats = Array(1, 1, 1), Sets = 3 },
+                new MultiRepeat { Percent = 0.75, Repeats = Array(1, 1, 1), Sets = 1 }
+            }
+        };
+
+        var ex2 = new Snatch(SnatchPull)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.80, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 0.90, Repeats = 2, Sets = 2 },
+                new SingleRepeat { Percent = 1.00, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 1.05, Repeats = 1, Sets = 2 }
+            }
+        };
+
+        var ex3 = new CleanAndJerk(FrontSquat)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.50, Repeats = 3, Sets = 2 },
+                new SingleRepeat { Percent = 0.60, Repeats = 3, Sets = 1 },
+                new SingleRepeat { Percent = 0.70, Repeats = 3, Sets = 1 },
+                new SingleRepeat { Percent = 0.80, Repeats = 2, Sets = 2 },
+                new SingleRepeat { Percent = 0.90, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 0.95, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 1.00, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 1.05, Repeats = 2, Sets = 1 }
             }
         };
 
