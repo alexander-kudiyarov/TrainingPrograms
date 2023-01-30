9f89a4bb201ec4c4d224bebf432c93d2964e2008
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2023-01-30 16:12:30 +0400

    MuscleGainProgram day 11

--- Kudiyarov.TrainingPrograms.Programs/MuscleGainProgram.cs
+++ Kudiyarov.TrainingPrograms.Programs/MuscleGainProgram.cs
@@ -19,7 +19,7 @@ public sealed class MuscleGainProgram : TrainingProgram
         {
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4,
-            GetSession3_1, GetSession3_2
+            GetSession3_1, GetSession3_2, GetSession3_3
         };
     }
 
@@ -647,6 +647,59 @@ public sealed class MuscleGainProgram : TrainingProgram
                 new StaticRepeat { Duration = TimeSpan.FromSeconds(40) },
                 new StaticRepeat { Duration = TimeSpan.FromSeconds(35) },
                 new StaticRepeat { Duration = TimeSpan.FromSeconds(30) }
+            }
+        };
+
+        var session = new Session
+        {
+            Rounds = new Round[]
+            {
+                new(ex1),
+                new(ex2),
+                new(ex3),
+                new(ex4)
+            }
+        };
+
+        return session;
+    }
+
+    private static Session GetSession3_3()
+    {
+        var ex1 = new Accessory(Hyperextension)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 15, Sets = 3 }
+            }
+        };
+
+        var ex2 = new MultiSnatch(HipSnatch, PushPress)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Percent = 0.5, Repeats = Array(2, 4), Sets = 2 },
+                new MultiRepeat { Percent = 0.6, Repeats = Array(1, 3), Sets = 3 }
+            }
+        };
+
+        var ex3 = new MultiSnatch(SnatchPull, HangSnatchPullBelowKnees)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Percent = 0.7, Repeats = Array(1, 5), Sets = 2 },
+                new MultiRepeat { Percent = 0.8, Repeats = Array(1, 3), Sets = 3 }
+            }
+        };
+
+        var ex4 = new CleanAndJerk(FrontSquat)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.6, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 0.7, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 0.8, Repeats = 2, Sets = 1 },
+                new SingleRepeat { Percent = 0.9, Repeats = 2, Sets = 3 }
             }
         };
 
