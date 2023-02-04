fa097a4d56c57d6c83ad162bfaa1725fe4a459c3
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2023-02-04 22:59:53 +0400

    MuscleGainProgram day 14

--- Kudiyarov.TrainingPrograms.Programs/MuscleGainProgram.cs
+++ Kudiyarov.TrainingPrograms.Programs/MuscleGainProgram.cs
@@ -20,7 +20,7 @@ public sealed class MuscleGainProgram : TrainingProgram
             GetSession1_1, GetSession1_2, GetSession1_3, GetSession1_4,
             GetSession2_1, GetSession2_2, GetSession2_3, GetSession2_4,
             GetSession3_1, GetSession3_2, GetSession3_3, GetSession3_4,
-            GetSession4_1
+            GetSession4_1, GetSession4_2
         };
     }
 
@@ -844,6 +844,68 @@ public sealed class MuscleGainProgram : TrainingProgram
                 new(ex3),
                 new(ex4),
                 new(4, ex5A, ex5B)
+            }
+        };
+
+        return session;
+    }
+
+    private static Session GetSession4_2()
+    {
+        var ex1 = new MultiCleanAndJerk(PowerClean, PushPress)
+        {
+            Repeats = new Repeat[]
+            {
+                new MultiRepeat { Percent = 0.5, Repeats = Array(2, 6), Sets = 3 }
+            }
+        };
+
+        var ex2 = new Snatch(SnatchPushPress)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.5, Repeats = 10, Sets = 2 },
+                new SingleRepeat { Percent = 0.6, Repeats = 8, Sets = 2 },
+                new SingleRepeat { Percent = 0.7, Repeats = 8, Sets = 2 },
+                new SingleRepeat { Percent = 0.8, Repeats = 6, Sets = 3 }
+            }
+        };
+        
+        var ex3 = new CleanAndJerk(GoodMorning)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Percent = 0.3, Repeats = 12, Sets = 4 }
+            }
+        };
+        
+        var ex4 = new Accessory(BoxJump)
+        {
+            Repeats = new Repeat[]
+            {
+                new SingleRepeat { Repeats = 8, Sets = 4 }
+            }
+        };
+
+        var ex5 = new Accessory(Plank)
+        {
+            Repeats = new Repeat[]
+            {
+                new StaticRepeat { Duration = TimeSpan.FromSeconds(60) },
+                new StaticRepeat { Duration = TimeSpan.FromSeconds(50) },
+                new StaticRepeat { Duration = TimeSpan.FromSeconds(40) }
+            }
+        };
+
+        var session = new Session
+        {
+            Rounds = new Round[]
+            {
+                new(ex1),
+                new(ex2),
+                new(ex3),
+                new(ex4),
+                new(ex5)
             }
         };
 
